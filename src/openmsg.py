d665b56a71e7ff9cc2bf4a59fca3fc4489d1406b
Clockworker <44556744+Ideefixze@users.noreply.github.com>
2020-02-29 15:28:17 +0100

    Settings reader done

--- src/openmsg.py
+++ src/openmsg.py
@@ -1,2 +1,29 @@
 import socket
 import sys
+import threading
+import time
+import read_settings as rs
+
+def print_thread (name, delay):
+    count = 0
+    while count < 10:
+        time.sleep(delay)
+        count += 1
+        print (name+": "+time.ctime(time.time()))
+
+
+
+rs.load_settings(r"server.txt");
+print (rs.settings["greeting_msg"])
+
+'''
+try:
+    print ("Starting threads...")
+    t1 = threading.Thread(target=print_thread,args=("A", 0.1)) 
+    t2 = threading.Thread(target=print_thread,args=("B", 0.15))
+    t1.start();
+    t2.start();
+except:
+    print ("Error: can't start threads")
+    '''
+
---
+++ src/read_settings.py
@@ -0,0 +1,38 @@
+import os
+import sys
+current_file_path = __file__
+current_file_dir = os.path.dirname(__file__)
+current_exe_dir = os.path.dirname(sys.executable)
+
+#dict that contains all loaded information from 
+#load_settings in form of [key=val]
+settings = dict()
+
+
+def load_settings(filename):
+    print(current_exe_dir)
+    print(current_file_dir)
+    print(current_file_path)
+
+    #determining whether we are running from .py or .exe
+    try:
+        settings_file = open(current_exe_dir+"\\"+filename,'r')
+    except:
+        settings_file = open(current_file_dir+"\\"+filename,'r')
+
+    while(settings_file.readable):
+        line = settings_file.readline()
+        line=line.replace('\n','')
+        setting=line.split('=',1)
+
+        if(setting[0]==''):
+            break
+
+        settings[setting[0]] = setting[1]
+
+    settings_file.close();
+
+    print("Loaded settings from " + filename)
+    for i in settings:
+        print (i+" = "+settings[i])
+
